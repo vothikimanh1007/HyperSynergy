f39509abbee0a1eed02f08af57ebd657a597ffa2
Võ Thị Kim Anh <160188340+vothikimanh1007@users.noreply.github.com>
2026-04-03 17:47:18 +0700

    Update __init__.py

--- hypersynergy/__init__.py
+++ hypersynergy/__init__.py
@@ -1,7 +1,8 @@
 from .models import MATG_Model
 from .losses import GraphFocalLoss
-from .data import DoTatLoiBenchmark
 from .explainers import NeuMapperExplainer
 from .evaluation import ModelEvaluator
+from .models import MATG_Model, SynergyPredictor
+from .data_loader import DoTatLoiBenchmark
 
 __version__ = "1.0.0"
